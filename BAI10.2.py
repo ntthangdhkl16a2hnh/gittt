293b1226c87ba6878bb3c3f8e851738dc1ff87ad
Nguyễn Thị Thanh Hằng <ntthang.dhkl16a2hn@sv.uneti.edu.vn>
2023-01-01 19:39:51 +0700

    Add files via upload

---
+++ BAI10.2.py
@@ -0,0 +1,8 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #bài 10.2
+>>> #vd x=2
+>>> x=2
+>>> print(abs(x))
+2
+>>> 
---
+++ bai10.1.py
@@ -0,0 +1,11 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #bài 10.1. ví du . cho x1 =2 ,x2=5, x3=9
+>>> x1=2
+>>> x2=5
+>>> x3=9
+>>> print(max(2,5,9))
+9
+>>> print(min(2,5,9))
+2
+>>> 
